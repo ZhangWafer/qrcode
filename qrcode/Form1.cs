afbc05c755d880f105677a7651a672b2e8797e98
ZhangWafer <ZhangWafer@users.noreply.github.com>
2018-11-10 15:39:05 +0800

    Initial commit

---
+++ qrcode/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using com.google.zxing;
+using com.google.zxing.qrcode;
+using com.google.zxing.common;
+using ByteMatrix = com.google.zxing.common.ByteMatrix;
+using EAN13Writer = com.google.zxing.oned.EAN13Writer;
+using EAN8Writer = com.google.zxing.oned.EAN8Writer;
+using MultiFormatWriter = com.google.zxing.MultiFormatWriter;
+
+
+namespace qrcode
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            string content = textBox1.Text;
+            ByteMatrix byteMatrix = new MultiFormatWriter().encode(content, BarcodeFormat.QR_CODE, 200, 200);
+            Bitmap bitmap = toBitmap(byteMatrix);
+            pictureBox1.Image = bitmap;
+            writeToFile(byteMatrix,ImageFormat.Jpeg,"D:\\Qrcode_pic\\qr01.jpg" );
+
+        }
+
+        public void WriteQrcodeToFile(string content,string filePath)
+        {
+            ByteMatrix byteMatrix = new MultiFormatWriter().encode(content, BarcodeFormat.QR_CODE, 200, 200);
+            Bitmap bitmap = toBitmap(byteMatrix);
+            pictureBox1.Image = bitmap;
+            writeToFile(byteMatrix, ImageFormat.Jpeg, filePath);
+
+        }
+
+        public static void writeToFile(ByteMatrix matrix, System.Drawing.Imaging.ImageFormat format, string file)
+        {
+            Bitmap bmap = toBitmap(matrix);
+            bmap.Save(file, format);
+        }
+        public static Bitmap toBitmap(ByteMatrix matrix)
+        {
+            int width = matrix.Width;
+            int height = matrix.Height;
+            Bitmap bmap = new Bitmap(width, height, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    bmap.SetPixel(x, y, matrix.get_Renamed(x, y) != -1 ? ColorTranslator.FromHtml("0xFF000000") : ColorTranslator.FromHtml("0xFFFFFFFF"));
+                }
+            }
+            return bmap;
+        }
+
+
+        public string recognitionQrcode(string filePath)
+        {
+            string qrcodeString="";
+
+
+            return qrcodeString;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (this.openFileDialog1.ShowDialog() != DialogResult.OK)
+            {
+                return;
+            }
+            Image img = Image.FromFile(this.openFileDialog1.FileName);
+            Bitmap bmap;
+            try
+            {
+                bmap = new Bitmap(img);
+            }
+            catch (System.IO.IOException ioe)
+            {
+                MessageBox.Show(ioe.ToString());
+                return;
+            }
+            if (bmap == null)
+            {
+                MessageBox.Show("Could not decode image");
+                return;
+            }
+            LuminanceSource source = new RGBLuminanceSource(bmap, bmap.Width, bmap.Height);
+            BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer(source));
+            Result result;
+            try
+            {
+                result = new MultiFormatReader().decode(bitmap);
+            }
+            catch (ReaderException re)
+            {
+                MessageBox.Show(re.ToString());
+                return;
+            }
+
+            MessageBox.Show(result.Text);
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+
+    }
+}
---
+++ qrcode/QrcodeClass.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using com.google.zxing;
+using com.google.zxing.qrcode;
+using com.google.zxing.common;
+using ByteMatrix = com.google.zxing.common.ByteMatrix;
+using EAN13Writer = com.google.zxing.oned.EAN13Writer;
+using EAN8Writer = com.google.zxing.oned.EAN8Writer;
+using MultiFormatWriter = com.google.zxing.MultiFormatWriter;
+
+namespace qrcode
+{
+    public class QrcodeClass
+    {
+
+        //生成二维码  content二维码内容,filePath二维码保存路径
+        public void WriteQrcodeToFile(string content, string filePath)
+        {
+            ByteMatrix byteMatrix = new MultiFormatWriter().encode(content, BarcodeFormat.QR_CODE, 200, 200);
+            Bitmap bitmap = toBitmap(byteMatrix);
+          //  pictureBox1.Image = bitmap;
+            writeToFile(byteMatrix, ImageFormat.Jpeg, filePath);
+
+        }
+
+
+        //识别二维码
+        public string recognitionQrcode(string filePath)
+        {
+            string qrcodeString = "";
+
+
+            return qrcodeString;
+        }
+
+
+
+
+        #region 私有类
+        private static void writeToFile(ByteMatrix matrix, System.Drawing.Imaging.ImageFormat format, string file)
+        {
+            Bitmap bmap = toBitmap(matrix);
+            bmap.Save(file, format);
+        }
+        private static Bitmap toBitmap(ByteMatrix matrix)
+        {
+            int width = matrix.Width;
+            int height = matrix.Height;
+            Bitmap bmap = new Bitmap(width, height, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    bmap.SetPixel(x, y, matrix.get_Renamed(x, y) != -1 ? ColorTranslator.FromHtml("0xFF000000") : ColorTranslator.FromHtml("0xFFFFFFFF"));
+                }
+            }
+            return bmap;
+        }
+
+        #endregion
+
+    }
+}
